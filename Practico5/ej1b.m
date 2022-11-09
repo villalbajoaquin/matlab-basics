a40db03a5d0c38f1c4975f2213b039858c87347d
villalbajoaquin <villalbajoaq@gmail.com>
2022-11-09 02:31:16 -0300

    feat: practico5/ej1b.m

---
+++ Practico5/ej1b.m
@@ -0,0 +1,10 @@
+clear; clc;
+
+% 1) b) Se desea graficar el siguiente conjunto de datos x = 1, 2, 3, 4, 5
+% e y = 0, 8,75, 400,7300. Utilizar escala logarítmica en el eje y, y
+% visualizar la rejilla de referencia.
+
+x = [1 2 3 4 5];
+y = [0, 8, 75, 400, 7300];
+
+semilogy(x, y), grid
