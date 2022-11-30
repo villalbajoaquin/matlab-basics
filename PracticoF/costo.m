077271270b3b55241875a43bf83bca4c80d1e762
villalbajoaquin <villalbajoaq@gmail.com>
2022-11-30 00:41:53 -0300

    final commit

--- PracticoF/costo.m
+++ PracticoF/costo.m
@@ -3,8 +3,8 @@
 % "ok", o el valor cero y la frase que explique el error.
 
 function [res] = costo(at, de, di, nm)
-% at = altura del tanque
-% de = d exterior entre 1,2 y 1,6 m
+% at = altura del tanque entre 2 y 4 m
+% de = d exterior entre 1 y 1,2 m
 % di = d interior entre 0,92*de y 0,96*de
 
 vc = pi.* at .*((de.^2)/4 - (di.^2)/4).*1000;
@@ -13,26 +13,40 @@ vf = pi.*((de - di).^2)/4.*de.*1000;
 % vc = volumen cilindro, multiplico por 1000 para la conversion en cm3
 vt = vf + vc; % volumen total
 
-if de <= 1.6 && de >= 1.2
-    if di <= 0.96*de && di >= 0.92*de
-        switch nm
-            case 1
-                peso = vt .* 14;
-                res = peso .* 8;
-                disp('ok')
-            case 2
-                peso = vt .* 9;
-                res = peso .* 7;
-            otherwise
-                res = 0;
-                disp('error: parametro de material no valido')
+if at <= 4 && at >= 2
+    if de <= 1.2 && de >= 1
+        if di <= 0.96*de && di >= 0.92*de
+            switch nm
+                case 1
+                    peso = vt .* 7.5;
+                    res = peso .* 4.3;
+                    disp('ok')
+                case 2
+                    peso = vt .* 6.9;
+                    res = peso .* 5.1;
+                    disp('ok')
+                case 3
+                    peso = vt .* 7.2;
+                    res = peso .* 4.8;
+                    disp('ok')
+                case 4
+                    peso = vt .* 7;
+                    res = peso .* 5;
+                    disp('ok')
+                otherwise
+                    res = 0;
+                    disp('error: parametro de material no valido')
+            end
+        else
+            res = 0;
+            disp('error: valor de diametro interior no valido')
         end
     else
         res = 0;
-        disp('error: valor de diametro interior no valido')
+        disp('error: valor de diametro exterior no valido')
     end
 else
     res = 0;
-    disp('error: valor de diametro exterior no valido')
+    disp('error: valor de altura no valido')
 end
 end
--- PracticoF/graficos2.m
+++ PracticoF/graficos2.m
@@ -3,16 +3,13 @@ clc; clear;
 % durante 6 horas. Se realizaron mediciones cada hora. Obtener los
 % siguientes graficos:
 
-t = 1:1:6;
-conc = [1.3 1.9 2.8 3.9 5 6.3];
-mcrorg = [1 5 32 1500 100000 130000];
-vol = [39 32 28 25 21 15];
+t = 5:5:30; % tiempo (hs)
+mcrorg = [10 300 2000 10000 20000 500000]; % num. microorganismos
+cons = [2 6 13 21 30 45]; % % de carbohidratos consumidos
 
-subplot(2, 2, 3), plot(t, conc, 'x:'), title('Concentracion'),
-xlabel('tiempo (hs.)'), ylabel('gr/cm3'), grid
+subplot(2, 1, 1), plot(t, cons, 'x:'), title('Carbohidratos Consumidos'),
+xlabel('tiempo (hora)'), ylabel('Porcentaje'), grid
 
-subplot(2, 2, 2), semilogy(t, mcrorg, '+:'), title('Microorganismos'),
-xlabel('tiempo (hs.)'), ylabel('Numero'), grid
-
-subplot(2, 2, 4), plot(t, conc, '*:'), title('Volumen'),
-xlabel('tiempo (hs.)'), ylabel('Litros'), grid
+subplot(2, 1, 2), semilogy(t, mcrorg, '*:'),
+title('Numero de Microorganismos'), xlabel('tiempo (hora)'),
+ylabel('Numero'), grid
